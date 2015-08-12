baebe25a1e4f2cd9fdf9416c4529263d5e74a516
Wiki Uploads <wiki@tampa.org>
2015-08-12 08:44:08 +0530

    Altert at intervals, popup an image, showing time to next altert.

---
+++ timer.sh
@@ -0,0 +1,8 @@
+for i in {1..9}
+do
+  eval argi="$"$i""
+  x=$((${argi:-30}*60));
+  text="Alarm in $x seconds / $(($x/60)) minutes'"
+  convert -pointsize 150 -fill red -draw "text 360,160 '$i'"  -pointsize 20 -draw "text 60,600 '$text'" magick:wizard - | display &
+  sleep $x
+done
