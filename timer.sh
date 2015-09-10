2fcffd7e56a82c23f652c207cdb9f9b13834dd46
Wiki Uploads <wiki@champa.org>
2015-09-11 05:06:16 +0530

    display requires - for stdin in new versions

--- timer.sh
+++ timer.sh
@@ -3,6 +3,6 @@ do
   eval argi="$"$i""
   x=$((${argi:-30}*60));
   text="Alarm in $x seconds / $(($x/60)) minutes'"
-  convert -pointsize 150 -fill red -draw "text 360,160 '$i'"  -pointsize 20 -draw "text 60,600 '$text'" magick:wizard - | display &
+  convert -pointsize 150 -fill red -draw "text 360,160 '$i'"  -pointsize 20 -draw "text 60,600 '$text'" magick:wizard - | display - &
   sleep $x
 done
